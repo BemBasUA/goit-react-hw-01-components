c6ad76e193313fc831c955435c110a72b3f876c6
Ihor Podvalniy <bembasua@gmail.com>
2022-09-23 23:34:15 +0300

    Завершує перше завдання

---
+++ src/components/Profile.jsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Box } from './Box';
+
+const Image = styled.img`
+  display: block;
+  max-width: 100px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const UserName = styled.p`
+  text-align: center;
+`;
+
+const Tag = styled.p`
+  text-align: center;
+`;
+
+const Location = styled.p`
+  text-align: center;
+`;
+
+const ListItem = styled.p`
+  margin: auto;
+  padding: 10px 0;
+  text-align: center;
+`;
+
+const Item = styled.p``;
+
+const ItemValue = styled.span``;
+
+export const Profile = ({ username, tag, location, stats, avatar }) => {
+  return (
+    <Box bg="primary" m="50px auto" width="cardWidth">
+      <Box>
+        <Image src={avatar}></Image>
+        <UserName>{username}</UserName>
+        <Tag>{tag}</Tag>
+        <Location>{location}</Location>
+      </Box>
+      <Box
+        as="ul"
+        display="flex"
+        padding="0"
+        margin="0"
+        bg="secondary"
+        justifyContent="space-evenly"
+      >
+        <ListItem>
+          <Item>Followers</Item>
+          <ItemValue>{stats.followers}</ItemValue>
+        </ListItem>
+        <ListItem>
+          <Item>Views</Item>
+          <ItemValue>{stats.views}</ItemValue>
+        </ListItem>
+        <ListItem>
+          <Item>Likes</Item>
+          <ItemValue>{stats.likes}</ItemValue>
+        </ListItem>
+      </Box>
+    </Box>
+  );
+};
+
+Profile.propTypes = {
+  avatar: PropTypes.string,
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    primary: '#D1D1D1',
+    secondary: '#747474',
+  },
+  sizes: {
+    cardWidth: 400,
+  },
+};
